a8be3aee71ebfb4c4ed37783fba2adf09e5a2777
Chris Arderne <chris@trigger.dev>
2026-08-13 11:44:39 +0100

    feat(webapp,clickhouse): add bounded global log search

--- internal-packages/clickhouse/src/index.ts
+++ internal-packages/clickhouse/src/index.ts
@@ -314,7 +314,8 @@ export class ClickHouse {
 
   get taskEventsSearch() {
     return {
-      logsListQueryBuilder: getLogsSearchListQueryBuilder(this.reader),
+      logsListQueryBuilder: (version: "v1" | "v2" = "v1") =>
+        getLogsSearchListQueryBuilder(this.reader, version)(),
     };
   }
 
